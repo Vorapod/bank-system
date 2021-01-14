44bdf5cfa9fba9a850ddbd6db4a35a2e31723eab
Vorapod <vorapod.e@gmail.com>
2021-01-14 12:16:36 +0700

    [Edit] add defalut value

--- BankSystem.Database/Account.sql
+++ BankSystem.Database/Account.sql
@@ -2,7 +2,7 @@
 (
 	[IBANNumber] NCHAR(18) NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
-    [IsActive] INT NOT NULL, 
+    [IsActive] BIT NOT NULL DEFAULT 1, 
     [Balance] DECIMAL NOT NULL, 
     [CreatedDate] DATETIME NOT NULL
 )
